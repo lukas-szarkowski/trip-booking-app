4ef295e2786a16c75bab8259be72691d4248aa0a
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-06-20 17:42:17 +0200

    add readFile and createNewExcursionEl functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,44 @@
-const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-"2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
+const uploaderEl = document.querySelector('.uploader');
+const ulExcursionEl = document.querySelector('.excursions');
+const liExcursionEl = document.querySelector('.excursions__item--prototype');
+const ulSummaryEl = document.querySelector('.panel__summary');
+const liSummaryEl = document.querySelector('.summary__item--prototype');
+const formExcursionEl = document.querySelector('.excursions');
+const orderTotalPriceEl = document.querySelector('.order__total-price-value');
+const formPanelOrderEl = document.querySelector('.panel__order');
 
-console.log( txt.split(/[\r\n]+/gm) );
+// functions
+
+function readFile (e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const content = event.target.result;
+        const excursionsList = content.split(/[\r\n]+/gm);
+        excursionsList.forEach(function (item) {
+            const newLiExcursionEl = createNewExcursionEl(item);
+            ulExcursionEl.appendChild(newLiExcursionEl);
+             
+        });
+    }
+
+    reader.readAsText(file, 'UTF-8');
+}
+
+function createNewExcursionEl (item) {
+    const excursionList = item.split(('","'));
+    const newLiExcursionEl = liExcursionEl.cloneNode(true);
+    liExcursionEl.classList.remove('excursions__item--prototype');
+    const title = newLiExcursionEl.querySelector('h2');
+    const pEl = newLiExcursionEl.querySelector('p');
+    const prices = newLiExcursionEl.querySelectorAll('.excursions__price');
+    title.innerText = excursionList[1];
+    pEl.innerText = excursionList[2];
+    prices[0].innerText = excursionList[3];
+    prices[1].innerText = excursionList[4].replace(/\"/g, '');
+    return newLiExcursionEl;
+}
+
+// Event Listeners
+
+uploaderEl.addEventListener('change', readFile);
